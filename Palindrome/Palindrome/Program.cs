63875c019963f33813b39136407df0e076b586a2
Yash Deshpande <ydeshpande@symmera.com>
2024-01-31 15:06:42 +0530

    second program

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        int result = 0;
+        int rem;
+
+        public int CheckPlaindromeNo(int n)
+        {
+            int temp = n;
+            while (n > 0)
+            {
+                rem = n % 10;
+                result = result * 10 + rem;
+                n = n / 10;
+            }
+            if (result == temp)
+            {
+                Console.WriteLine("PalindromeNo");
+            }
+            else
+            {
+                Console.WriteLine("Not a PalindromeNo");
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program(); 
+            int number = 122; 
+            program.CheckPlaindromeNo(number);
+            Console.ReadLine();
+        }
+    }
+}
