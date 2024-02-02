1594cb6a4915aad4e64b1f98a57de21e6a2effc8
Yash Deshpande <ydeshpande@symmera.com>
2024-02-02 17:09:09 +0530

    task 3

---
+++ 1 Feb 2024/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] a = { 1, 2, 3, 4, -10 };
+            int sum = 0, max = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                sum += a[i];
+                max = Math.Max(sum, max);
+
+                if (sum <= 0)
+                {
+                    sum = 0;
+                }
+
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+
+       
+    }
+
+}
