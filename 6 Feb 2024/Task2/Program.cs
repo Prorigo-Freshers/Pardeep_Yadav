3dec7ce2ad49d0f72b767dbb100c05544f403859
Yash Deshpande <ydeshpande@symmera.com>
2024-02-08 19:19:30 +0530

    Find all character count of given string

---
+++ 6 Feb 2024/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "hello";
+            while (input.Length > 0)
+            {
+                Console.Write(input[0] + " = ");
+
+
+
+                int count = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[0] == input[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                input = input.Replace(input[0].ToString(), string.Empty);
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
