69fb5c76926e6facd5c05b8a37531cf2a41de19c
Yash Deshpande <ydeshpande@symmera.com>
2024-02-08 18:08:16 +0530

    finding out the minimum number of operations required such that the maximum element of the resulting array

---
+++ 7 Feb 2024/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 3, 4, 5 };
+            int targetValue = 3;
+            int Count = 0;
+            bool isValuePresent=false;
+
+            for(int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > targetValue)
+                {
+                    Count++;
+                }
+
+                if (A[i] == targetValue)
+                {
+                    isValuePresent = true; 
+                }
+
+
+           
+            }
+
+            if (isValuePresent)
+            {
+                Console.WriteLine(Count);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+         
+
+          
+            Console.ReadLine();
+        }
+   
+
+    }
+
+
+}
