3692d84cc71fb173213ebe2fba8dc25c96a11e95
Yash Deshpande <ydeshpande@symmera.com>
2024-02-09 10:00:13 +0530

    find no is even or odd

---
+++ 8 Feb 2024/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static bool IsEven(int num)
+    {
+        return (num & 1) == 0;
+    }
+
+    static void Main(string[] args)
+    {
+        int number = 11;
+        if (IsEven(number))
+        {
+            Console.WriteLine($"{number} is even.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is odd.");
+        }
+
+
+        Console.ReadLine();
+    }
+}
