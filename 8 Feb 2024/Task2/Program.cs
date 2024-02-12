8a9938132b32e42b789b7b213a9484246d40397d
Yash Deshpande <ydeshpande@symmera.com>
2024-02-12 11:09:34 +0530

    Given a string s, find the length of the longest substring without repeating characters.

---
+++ 8 Feb 2024/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LongestSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcabcbb";
+            //string s = "bbbbb";
+            //string s = "pwwkew";
+
+            int maxLength = 0;
+            int left = 0;
+            int right = 0;
+
+            bool[] visited = new bool[128];
+
+            while (right < s.Length)
+            {
+                if (!visited[s[right]])
+                {
+                    visited[s[right]] = true;
+                    right++;
+                    maxLength = Math.Max(maxLength, right - left);
+                }
+                else
+                {
+                    visited[s[left]] = false;
+                    left++;
+                }
+            }
+
+            Console.WriteLine(maxLength);
+        }
+    }
+}
