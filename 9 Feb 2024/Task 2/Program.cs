0755b3fbb056360edcc131b45d2da7ecd59070ae
Yash Deshpande <ydeshpande@symmera.com>
2024-02-12 11:23:40 +0530

    You are given an array of integers A of size N.Return the difference between the maximum among all even numbers of A and the minimum among all odd numbers in A

---
+++ 9 Feb 2024/Task 2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+using System;
+
+namespace DifferenceBetweenMaxEvenNum_MinOddNum
+{
+    internal class DifferenceBetweenMaxEvenNumMinOddNum
+    {
+        static void Main(string[] args)
+        {
+            // int[] arr = { 0, 2, 9 };
+            int[] arr = { 5, 17, 100, 1 };
+
+            int diff = differenceBtwMaxEvenAndMinOdd(arr);
+            Console.WriteLine(diff);
+        }
+
+        public static int differenceBtwMaxEvenAndMinOdd(int[] arr)
+        {
+            int maxEven = int.MinValue;
+            int minOdd = int.MaxValue;
+
+
+            foreach (int n in arr)
+            {
+                if (n % 2 == 0 && n > maxEven)
+                {
+                    maxEven = n;
+                }
+
+                else if (n % 2 != 0 && n < minOdd)
+                {
+                    minOdd = n;
+                }
+
+            }
+
+            int diff = maxEven - minOdd;
+            return diff;
+
+        }
+    }
+}
